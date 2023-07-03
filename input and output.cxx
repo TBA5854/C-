9a1f06088c5d0e94b61b6e9909e747654be83fca
TBA5854 <adhavan5030@gmail.com>
2023-07-04 01:42:54 +0530

    pushing local files

---
+++ input and output.cxx
@@ -0,0 +1,10 @@
+#include<iostream.h>
+#include<stdio.h>
+int main()
+{
+	int a=0 ,b=0;
+	cout<<"Hello World";
+	cin>>a>>b;
+	cout<<a*b;
+	return 0;
+}
---
+++ while loop.cxx
@@ -0,0 +1,11 @@
+#include<iostream.h>
+#include<stdio.h>
+int main()
+{
+	int a=50 , sum = 50;
+	while(a<100){
+		a++;
+		sum=sum+a;
+	}
+	cout<<sum<<endl<<75*51;
+}
